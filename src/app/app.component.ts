4aa4c3cd6f5e71fc8da7ceebf93ee2c3ac9739c1
Rodrigo Rocha <rodrigo.desenvolvedor.web@gmail.com>
2022-11-28 02:47:37 -0300

    feat: include appInput component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { appInput } from 'dawn-js-ui';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-dawn-js-ui';
+
+  constructor(private dom: ElementRef) {}
+
+  ngOnInit () {
+    const context = this.dom.nativeElement.querySelctor
+    appInput.register('ds-input', this.dom.nativeElement)
+    appInput.props.set({ label: 'Nome'})
+    appInput.init()
+  }
 }
